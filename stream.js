aa470e2dcb00c1524342c90dd7cb6f15fbc30528
Dima Maltsev <maltsev@fishsticksgames.com>
2013-05-24 02:40:20 +0300

    new post

--- stream.js
+++ stream.js
@@ -20,7 +20,8 @@ var dataStream = {
 											" <p> So here we gonna meet the deadline at 5 AM of Saturday's morning. </p> " +
 											" <p>Im not worried about this competition 'cause as always, this is almost unreal to win something serious with the game created in two days.</ps>" + 
 											" <p>But in case we win there, there will be a great chance for us to establish our own game dev production... </p>" + 
-											" <p>Here is a link to <a target='_blank' href='http://dl.dropboxusercontent.com/u/2959036/sex/index.html'>our Game </a>. So u can take a look at it if u want :)",
+											" <p>Here is a link to <a target='_blank' href='http://dl.dropboxusercontent.com/u/2959036/sex/index.html'>our Game </a>. So u can take a look at it if u want :)</p>" +
+											" <p> <a target='_blank' href='http://www.gamesforchange.org/2013/04/enter-the-sex-etc-game-design-contest-to-win-35000/'>Game Conf link </a></p> ",
 						images  :['condoms.png']
 						}
 				]
