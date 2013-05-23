e947aaeba0a21530815581c8255bc873d47ca87f
Dima Maltsev <maltsev@fishsticksgames.com>
2013-05-24 02:33:43 +0300

    deadline date

--- stream.js
+++ stream.js
@@ -16,7 +16,7 @@ var dataStream = {
 						caption :"Game Dev Competition",
 						text    : "<p>Well. The first thing i wanna share is ours Game Dev competition participation.</p>" + 
 											" <p>Me and my friend Danyl Dantsev are taking part in Game development competition that is dedicated on sex awareness theme." + 
-											" Competition deadline is almost here - 22th of May (U.S. time).</p> " +
+											" Competition deadline is almost here - 25th of May (U.S. time).</p> " +
 											" <p> So here we gonna meet the deadline at 5 AM of Saturday's morning. </p> " +
 											" <p>Im not worried about this competition 'cause as always, this is almost unreal to win something serious with the game created in two days.</ps>" + 
 											" <p>But in case we win there, there will be a great chance for us to establish our own game dev production... </p>" + 
